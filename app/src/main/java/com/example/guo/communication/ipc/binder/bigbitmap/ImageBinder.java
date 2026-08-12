93df28cd884a8c1f1eea5510fd362507523d5d4c
Darren.eth <bagguo23@gmail.com>
2026-08-12 19:07:50 +0800

    annotation

--- app/src/main/java/com/example/guo/communication/ipc/binder/bigbitmap/ImageBinder.java
+++ app/src/main/java/com/example/guo/communication/ipc/binder/bigbitmap/ImageBinder.java
@@ -3,6 +3,9 @@ package com.example.guo.communication.ipc.binder.bigbitmap;
 import android.graphics.drawable.Drawable;
 import android.os.Binder;
 
+/**
+ * 继承binder
+ */
 public class ImageBinder extends Binder {
     private final Drawable drawable;
 
